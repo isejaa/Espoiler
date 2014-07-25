72b3e8925f3336d5202b7a88fad95849412fd1f3
Humberto <Hawksmoor@gmail.com>
2014-07-25 18:37:05 +0200

    angular cookies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('Espoiler', ['ngRoute', 'ngResource','mgcrea.ngStrap'])
+angular.module('Espoiler', ['ngRoute', 'ngResource', 'ngCookies','mgcrea.ngStrap'])
   .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     'use strict';
     $locationProvider.html5Mode(true);
---
+++ app/scripts/controllers/loginController.js
@@ -0,0 +1,9 @@
+angular.module('Espoiler')
+  .controller('LoginController', ['$scope', 'Auth', function($scope, Auth){
+      $scope.login = function() {
+        Auth.login({
+          email: $scope.email,
+          password: $scope.password
+        });
+      };
+  }]);
